c22e9b44560293f6e5ea661a1f963be16ae07e22
davlloyd <davidlloyd0@gmail.com>
2022-09-01 19:03:53 +1000

    Made Aussie default

--- surferslookout/main/views.py
+++ surferslookout/main/views.py
@@ -19,7 +19,7 @@ def home():
 def sites():
     app.logger.info("Accessing sites page")
     _countries = Country.query.all()
-    _countryid = 0
+    _countryid = 1
     _states={}
     _stateid = 0
     _regions={}
