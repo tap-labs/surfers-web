45e27b10a0709e6bc4bdc634ddf0e1732482aa5e
davlloyd <davidlloyd0@gmail.com>
2022-09-20 21:38:39 +1000

    test

--- surfersweb/config.py
+++ surfersweb/config.py
@@ -4,7 +4,7 @@ from pyservicebinding import binding
 basedir = os.getcwd()
 
 class Config:
-    VERSION = '15'
+    VERSION = '16'
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret'
     SESSION_COOKIE_HTTPONLY = False
     GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
